5230fda7b3e62b1e1677e82b35b20be01dc53c73
JoshSevy <joshuasevy@outlook.com>
2021-03-02 18:53:57 -0700

    Layout testbed for courses service

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,6 +1,28 @@
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { CoursesService } from "./courses.service";
 
 
+describe("CoursesService", () => {
+    let coursesService: CoursesService;
+    httpTestingController: HttpTestingController;
 
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                CoursesService
+            ]
+        })
+
+        coursesService = TestBed.inject(CoursesService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    })
+
+    it('should retrieve all courses', () => {
+
+    });
+});
 
 
 
