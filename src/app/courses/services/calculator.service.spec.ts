57ba7acde530a0f44bdf5969ca3dcb3ca13471e4
JoshSevy <joshuasevy@outlook.com>
2021-03-02 18:02:59 -0700

    Add tests to calculater service

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,18 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe('CalcuatorService', () => {
+    it('should add two numbers', () => {
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
+    });
+
+    it('should subtract two numbers', () => {
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.subtract(2, 1);
+
+        expect(result).toBe(1);
+    });
+});
