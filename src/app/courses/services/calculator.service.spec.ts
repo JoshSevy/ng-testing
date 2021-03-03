282c931dfed17ecefdf1f4f6a2d16adae3bf21d6
JoshSevy <joshuasevy@outlook.com>
2021-03-02 18:34:36 -0700

    Add testbed to testing for calc service

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,21 +1,32 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 describe('CalcuatorService', () => {
+    let calculator: CalculatorService,
+    loggerSpy: any;
+
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                { provide: LoggerService, useValue: loggerSpy }
+            ]
+        })
+        calculator = TestBed.inject(CalculatorService);
+    });
+
     it('should add two numbers', () => {
-        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
-
-        const calculator = new CalculatorService(logger);
         const result = calculator.add(2, 2);
-
         expect(result).toBe(4, "unexpected addition result");
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract two numbers', () => {
-        const calculator = new CalculatorService(new LoggerService());
         const result = calculator.subtract(2, 1);
-
         expect(result).toBe(1, "unexpected subtraction result");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 });
