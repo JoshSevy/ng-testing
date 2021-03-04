e41a8d845af69e7a8c66822f0887035cf0f6fae3
JoshSevy <joshuasevy@outlook.com>
2021-03-03 18:36:08 -0700

    Setup testing file for cardlistcomponent

---
+++ src/app/courses/courses-card-list/courses-card-list.spec.ts
@@ -0,0 +1,16 @@
+
+
+describe('CourseCardListComponoent', () => {
+
+  it('should create the component', () => {
+
+  });
+
+  it('should display the course list', () => {
+
+  });
+
+  it('should display the first course', () => {
+
+  });
+})
