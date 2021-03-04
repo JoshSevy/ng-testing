ef78b1589db23125e330f3e9cae15ac0b0a7d3e8
JoshSevy <joshuasevy@outlook.com>
2021-03-03 18:48:49 -0700

    Add first test that checks if instance of component created

--- src/app/courses/courses-card-list/courses-card-list.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.spec.ts
@@ -1,16 +1,32 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CoursesModule } from "../courses.module";
+import { CoursesCardListComponent } from "./courses-card-list.component";
 
 
 describe('CourseCardListComponoent', () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(async() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule]
+    })
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(CoursesCardListComponent);
+      component = fixture.componentInstance;
+    });
+  })
 
   it('should create the component', () => {
-
+    expect(component).toBeTruthy();
   });
 
   it('should display the course list', () => {
-
+    pending();
   });
 
   it('should display the first course', () => {
-
+    pending();
   });
 })
