e7332cf4ffa9334b8bfe7cd6f1870759f6dabddb
JoshSevy <joshuasevy@outlook.com>
2021-03-04 19:56:35 -0700

    tested all tab instances on home

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -18,6 +18,8 @@ describe('HomeComponent', () => {
 
   const beginnerCourses = setupCourses()
     .filter(course => course.category === "BEGINNER");
+  const advancedCourses = setupCourses()
+    .filter(course => course.category === "ADVANCED");
 
   beforeEach(async() => {
 
@@ -46,17 +48,28 @@ describe('HomeComponent', () => {
   it('should display only beginner courses', () => {
     coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
 
-      fixture.detectChanges();
+    fixture.detectChanges();
 
-      const tabs = el.queryAll(By.css(".mat-tab-label"));
-      expect(tabs.length).toBe(1, "Unexpected number of tabs found");
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
   });
 
   it('should display only advanced courses', () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
+
   });
 
   it('should display both tabs', () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(2, "Expected 2 tabs to be found");
   });
 })
